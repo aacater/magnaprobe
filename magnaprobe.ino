17be983f02789bc28d2035a57fc3dfa7d0a26750
aacater <aacater@net.retac.me>
2026-04-27 08:46:09 -0800

    delete sdcard.ino

--- magnaprobe.ino
+++ magnaprobe.ino
@@ -5,16 +5,19 @@
 // #include "sdcard.ino"
 #include "BNO086.ino"
 #include "ADC.ino"
+#include "sdcard.h"
+
+MagnaSD sd;
 
 void setup(void) {
   Serial.begin(115200);
   Wire.begin();
   
-  // setupSD();
   setupIMU();
   setupACD();
+  sd.SDset();
 }
 
 void loop(void) {
-
+  sd.record();
 }
