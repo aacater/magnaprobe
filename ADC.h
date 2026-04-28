73bc52752f6da12df041016cfa7d99dc775c3340
gfmarson-a11y <gfmarson@alaska.edu>
2026-04-27 20:13:20 -0800

    Update ADC.h

--- ADC.h
+++ ADC.h
@@ -1,21 +1,29 @@
-/*
-  ADC.h
-  by Griffin Marson
-*/
-
-#ifndef ADC_h
-#define ADC_h
+#ifndef AD7791_h
+#define AD7791_h
 
 #include <Arduino.h>
-#include <SPI.h>
-#include "AD7791.h"
 
-#define ADC_CS_PIN   8
-#define ADC_SCK_PIN  18
-#define ADC_MISO_PIN 19
-#define ADC_MOSI_PIN 23
+#define MODE_WRITE    0x10
+#define FILTER_WRITE  0x20
+#define STATUS_READ   0x08
+#define MODE_READ     0x18
+#define FILTER_READ   0x28
+#define DATA_READ     0x38
+#define RESET         0xFF
+#define AD7791_SS     4
 
-uint32_t readADCData(void);
-void setupADC(void);
+extern float currentDepth;
+
+class AD7791class {
+  public:
+    void ADCsetup();
+    void ADCrecord();
+    void writeAd7791(uint8_t ui8address, uint8_t ui8value);
+    uint32_t readAd7791(uint8_t ui8address);
+    void AD7791_SPI_Configuration(void);
+    float rawToDepth(uint32_t raw);
+};
+
+extern AD7791class AD7791;
 
 #endif
